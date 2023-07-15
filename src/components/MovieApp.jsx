cc18ef63feacea7157e9d03170cdcf861efcc3b8
soumya <117825720+soumya3969@users.noreply.github.com>
2023-07-15 11:14:48 +0000

    final commit

--- src/components/MovieApp.jsx
+++ src/components/MovieApp.jsx
@@ -7,7 +7,7 @@ const MovieApp = () => {
     const [movies, setMovies] = useState([]);
     const [featuredMovie, setFeaturedMovie] = useState(null);
 
-    const haldleSearch = async () => {
+    const handleSearch = async () => {
         const response = await fetch(
             `https://www.omdbapi.com/?apikey=d9b316c&s=${searchTerm}`
         );
@@ -32,7 +32,7 @@ const MovieApp = () => {
 
             if (movies.length > 0) {
                 const randomIndex = Math.floor(Math.random() * movies.length);
-                const randomMovies = movies[randomIndex];
+                const randomMovie = movies[randomIndex];
 
                 const movieResponse = await fetch(
                     `https://www.omdbapi.com/?apikey=d9b316c&i=${randomMovie.imdbID}`
@@ -47,18 +47,57 @@ const MovieApp = () => {
             }
         }
     };
-    useEffect(()=>{
+    useEffect(() => {
         fetchRandomMovies();
-    },[]);
+    }, []);
 
-    return(
+    return (
         <div>
             <header>
                 <h1>Movie App</h1>
                 <div id="search-container">
-                    <input type="text" id="search-input" placeholder="Search By Movie Name" value={searchTerm}  />
+                    <input type="text" id="search-input" placeholder="Search By Movie Name" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+                    <button id="search-button" onClick={handleSearch}>Search</button>
                 </div>
             </header>
+            <main>
+                <section id="home">
+                    <center><h2>For You</h2></center>
+                    <div id="featured-movie">
+                        {featuredMovie &&(
+                            <div className="movie-card">
+                                <img src={
+                                    featuredMovie.Poster !== 'N/A'
+                                    ? featuredMovie.Poster
+                                    : 'placeholder.png'
+                                } alt={featuredMovie.Title} />
+                                <h3>{featuredMovie.Title}</h3>
+                                <p>Year:{featuredMovie.Year}</p>
+                                <p>Rated: {featuredMovie.Rated}</p>
+                                <p>Runtime: {featuredMovie.Runtime}</p>
+                                <p>Genre: {featuredMovie.Genre}</p>
+                            </div>
+                        )}
+                    </div>
+                </section>
+                <section id="search-results">
+                    <h2>Search Results</h2>
+                    <div id="movie-container">
+                        {movies.map((movie)=>(
+                            <div className="movie-card" key={movie.imdbID}>
+                                <img 
+                                src={movie.Poster !== 'N/A' ? movie.Poster : 'placeholder.png'} 
+                                alt={movie.Title} 
+                                />
+                                <h3>{movie.Title}</h3>
+                                <p>Year: {movie.Year}</p>
+                            </div>
+                        ))}
+                    </div>
+                </section>
+            </main>
         </div>
-    )
-}
+    );
+};
+
+export default MovieApp;
